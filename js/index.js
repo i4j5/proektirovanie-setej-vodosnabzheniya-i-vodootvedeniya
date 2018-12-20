e5bc0f5d4e2e7af50729619833ea145c39b24d76
Aleksandr Sazhin <i4j5@ya.ru>
2018-12-20 15:18:06 +0300

    -

--- js/index.js
+++ js/index.js
@@ -26,6 +26,42 @@
 
 $(document).ready(()=>{
 
+	let $items = $('.tabs__item')
+	$items.each((index, el) => {
+		let $el = $(el)
+		$el.children('.tabs__title').click(function(event) {
+			$el.toggleClass('tabs__item_active')
+
+			let $progressBar = $el.children('.tabs__text').children('.progress-bar')
+			let interest = $progressBar.data('interest')
+
+			
+			if ( $el.hasClass('tabs__item_active') ) {
+				$progressBar.children('.progress-bar__pace').children('.progress-bar__interest').text('')
+				$progressBar.children('.progress-bar__pace').width('0%')
+				$el.children('.tabs__text').slideDown(500)
+				setTimeout(() => {
+					$progressBar.children('.progress-bar__pace').width(interest + '%')
+					let n = 0
+					let timerId = setInterval(() => {
+						++n
+						$progressBar.children('.progress-bar__pace').children('.progress-bar__interest').text(n + '%')
+						// $progressBar.children('.progress-bar__pace').width(n + '%')
+					}, 1000 / interest)
+
+					setTimeout(() => {
+						clearInterval(timerId)
+					},1000)
+				}, 500)
+			} else {
+				$el.children('.tabs__text').slideUp(500)
+				setTimeout(() => {
+					$progressBar.children('.progress-bar__pace').width('0%')
+				}, 500)
+			}
+		});	
+	})
+
   	let now = new Date("December 31, 2018");
 
 	let endTS = now.getTime();
@@ -62,13 +98,18 @@ $(document).ready(()=>{
 		/// Баланс водопотребления и водоотведения
 		let product = $(this).data('product')
 		$('#product-input').val(product)
-		yatarget = 'order'
+		yatarget = 'price'
 		$('#modal__product').openModal()
 	})
 
 	$('.order').click(function(event) {
 		yatarget = 'advice'
 		$('#modal__order').openModal()
+	})
+
+	$('#question').click(function(event) {
+		yatarget = 'question'
+		$('#modal__question').openModal()
 	})
 
 	$('.zoom').click(function(event) {
@@ -131,7 +172,7 @@ $(document).ready(()=>{
 	           } else {
            			$('#modal__ok').openModal()
 	           }
-	           yaCounter51637940.reachGoal(yatarget)
+	           yaCounter51650636.reachGoal(yatarget)
 	           yatarget = 'stock'
 	        })
 	        .always(function() {
--- tmp/index.js
+++ tmp/index.js
@@ -25,6 +25,41 @@
 
 $(document).ready(function () {
 
+	var $items = $('.tabs__item');
+	$items.each(function (index, el) {
+		var $el = $(el);
+		$el.children('.tabs__title').click(function (event) {
+			$el.toggleClass('tabs__item_active');
+
+			var $progressBar = $el.children('.tabs__text').children('.progress-bar');
+			var interest = $progressBar.data('interest');
+
+			if ($el.hasClass('tabs__item_active')) {
+				$progressBar.children('.progress-bar__pace').children('.progress-bar__interest').text('');
+				$progressBar.children('.progress-bar__pace').width('0%');
+				$el.children('.tabs__text').slideDown(500);
+				setTimeout(function () {
+					$progressBar.children('.progress-bar__pace').width(interest + '%');
+					var n = 0;
+					var timerId = setInterval(function () {
+						++n;
+						$progressBar.children('.progress-bar__pace').children('.progress-bar__interest').text(n + '%');
+						// $progressBar.children('.progress-bar__pace').width(n + '%')
+					}, 1000 / interest);
+
+					setTimeout(function () {
+						clearInterval(timerId);
+					}, 1000);
+				}, 500);
+			} else {
+				$el.children('.tabs__text').slideUp(500);
+				setTimeout(function () {
+					$progressBar.children('.progress-bar__pace').width('0%');
+				}, 500);
+			}
+		});
+	});
+
 	var now = new Date("December 31, 2018");
 
 	var endTS = now.getTime();
@@ -70,13 +105,18 @@ $(document).ready(function () {
 		/// Баланс водопотребления и водоотведения
 		var product = $(this).data('product');
 		$('#product-input').val(product);
-		yatarget = 'order';
+		yatarget = 'price';
 		$('#modal__product').openModal();
 	});
 
 	$('.order').click(function (event) {
 		yatarget = 'advice';
 		$('#modal__order').openModal();
+	});
+
+	$('#question').click(function (event) {
+		yatarget = 'question';
+		$('#modal__question').openModal();
 	});
 
 	$('.zoom').click(function (event) {
@@ -136,7 +176,7 @@ $(document).ready(function () {
 					} else {
 						$('#modal__ok').openModal();
 					}
-					yaCounter51637940.reachGoal(yatarget);
+					yaCounter51650636.reachGoal(yatarget);
 					yatarget = 'stock';
 				}).always(function () {
 					//btn.val(btnText)
